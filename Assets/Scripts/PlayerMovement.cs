f084d12e2195f90c7dfc455440b9d645a76bdfd4
angyice <leonardo_villa@studenti.naba.it>
2021-01-08 16:19:51 +0100

    Input Set

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
+    private Vector3 moveVector;
+
     private float speed = 5.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
 
     void Start()
     {
@@ -15,6 +19,28 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
-        controller.Move((Vector3.forward * speed) * Time.deltaTime );
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        
+        }
+
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime; 
+        }
+
+        //X - Left & Right 
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+
+        //Y - Up & Down
+        moveVector.y = verticalVelocity;
+
+        //Z - Forward & Backward
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
     }
 }
