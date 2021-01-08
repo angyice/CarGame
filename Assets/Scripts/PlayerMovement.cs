73a6e5fda9bd1af7c52a418ba28bca325be05f1f
angyice <leonardo_villa@studenti.naba.it>
2021-01-08 14:42:36 +0100

    Player Set

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 5.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        controller.Move((Vector3.forward * speed) * Time.deltaTime );
+    }
+}
