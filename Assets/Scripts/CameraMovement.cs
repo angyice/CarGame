a6f4cadab442da690ab6c50e1988ebcccca0486d
angyice <leonardo_villa@studenti.naba.it>
2021-01-08 16:58:41 +0100

    Camera Set

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float zFocus;
+    public float xCameraAdjust;
+    public float yCameraAdjust;
+    public float zCameraAdjust;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        zFocus = GameObject.FindGameObjectWithTag("Player").transform.position.z;
+        this.transform.position = new Vector3(xCameraAdjust, yCameraAdjust, zFocus + zCameraAdjust);
+    }
+
+}
